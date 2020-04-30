caaabcea761501a23e52d6719d68ba085cfa2459
carloshermoso64 <59064594+carloshermoso64@users.noreply.github.com>
2020-04-30 14:29:29 +0200

    Creado JuegoManager y añadido los metodos de Usuario en la interfaz

---
+++ src/main/java/edu/upc/dsa/util/JuegoManager.java
@@ -0,0 +1,24 @@
+package edu.upc.dsa.util;
+
+import edu.upc.dsa.models.Usuario;
+
+public interface JuegoManager {
+
+    //Usuario
+
+    public Usuario getUsuario(String id);
+    public void addUsuario (Usuario u);
+    public void addUsuario(String nombre, String correo, String contraseña);
+    public void updateUsuario(Usuario u);
+    public boolean logInUsuario(String nombre, String contraseña);
+    public void deleteUsuario(Usuario u);
+
+    //Jugador
+
+    //Mapa
+
+    //Objetos
+
+    //Partida
+
+}
