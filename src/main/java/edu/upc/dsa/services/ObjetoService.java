a85c0bc93193d4172b596b13e4517939dcc84a0b
anasamlou <61283934+anasamlou@users.noreply.github.com>
2020-05-02 03:11:25 +0200

    userdatamanager + post y put de usuarioService

---
+++ src/main/java/edu/upc/dsa/services/ObjetoService.java
@@ -0,0 +1,36 @@
+import edu.upc.dsa.models.Objeto;
+import edu.upc.dsa.models.Usuario;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Api(value = "/objetos", description = "Endpoint de objetos")
+@Path("/objetos")
+public class ObjetoService {
+
+    private UserDaoImp db; // Una vez se instale la librería de sql será el encargado
+
+    @GET // GET a specific user that has an id
+    @ApiOperation(value = "get objetos by username", notes = "get objetos by username")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Successful", response = Objeto.class),
+            @ApiResponse(code = 404, message = "User not found")
+    })
+    @Path("/User/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getObjects(@PathParam("username") String username) {
+        Usuario u = this.db.getUser(username);
+        if (u == null) {
+            return Response.status(404).build();
+        } else return Response.status(201).entity(u).build();
+    }
+}
+
--- src/main/java/edu/upc/dsa/services/UsuarioService.java
+++ src/main/java/edu/upc/dsa/services/UsuarioService.java
@@ -1,15 +1,13 @@
 package edu.upc.dsa.services;
 
+import edu.upc.dsa.models.UserDataManager;
 import edu.upc.dsa.models.Usuario;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -34,6 +32,42 @@ public class UsuarioService {
         }
         else return Response.status(201).entity(u).build();
     }
+
+    @POST
+    @ApiOperation(value = "add new usuario", notes = "Adding new user to database")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Successful", response= Usuario.class),
+            @ApiResponse(code = 500, message = "Validation Error")
+    })
+    @Path("/usuario/")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response addUser(@PathParam("AddUser") UserDataManager addingUser) {
+
+        if(addingUser == null){
+            return Response.status(500).build();
+        }
+        Usuario u = this.bd.addUser(addingUser.getName(),addingUser.getEmail(),addingUser.getPassword());
+        return Response.status(201).entity(u.getId()).build();
+    }
+
+    @PUT
+    @ApiOperation(value = "update User data", notes = "updated")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Successful",response = Usuario.class),
+            @ApiResponse(code = 404, message = "User not found")
+    })
+    @Path("/usuario/{name}")
+    public Response updateUser(UserDataManager updatingUser) {
+
+        Usuario u = this.bd.getUser(updatingUser.getName());
+
+        if (u == null) return Response.status(404).build();
+        else {
+            this.bd.updateUser(updatingUser.getName(),updatingUser.getEmail(),updatingUser.getPassword());
+            return Response.status(201).entity(u).build();
+        }
+    }
+
 
 
 
